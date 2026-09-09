ca84342499f70a6e472fb3f251f3f5683a13bb58
kianfarzaneh <kianfarzaneh1380@gmail.com>
2026-09-09 15:59:33 +0330

    fix: change text of cofirm and cancel delete buttons.

--- src/components/contactList/deleteModal/deleteModal.jsx
+++ src/components/contactList/deleteModal/deleteModal.jsx
@@ -13,13 +13,13 @@ const DeleteModal = ({modalState,closeModalBtn,contactId,onDeleteSuccess}) => {
 
     return <div className="bg-black bg-opacity-50 fixed inset-0 flex justify-center items-center z-50">
         <div className="bg-white p-4">
-            <h1 className="p-4 font-black text-[20px]">آر یو شُر یو وانت  تو حذفینگ دیس وامونده</h1>
+            <h1 className="p-4 font-black text-[20px]">آیا مطمئن هستید از حذف این مخاطب</h1>
             <div className="flex justify-center items-center gap-3">
-                <button onClick={handleDeleteContact} className="bg-red-600 text-white p-2 rounded-md">
-                    دکمه ی سیکشو بزن
-                </button>
                 <button onClick={closeModalBtn} className="bg-blue-500 text-white p-2 rounded-md">
-                    باو ولش کن
+                   خیر
+                </button>
+                <button onClick={handleDeleteContact} className="bg-red-600 text-white p-2 rounded-md">
+                   بله 
                 </button>
             </div>
         </div>
